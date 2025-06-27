39847d60da892a81dab2c229aaa9c09cbc68931d
Ammar Alkady <110333172+AmmarAlkady49@users.noreply.github.com>
2025-06-27 14:53:16 +0300

    Initial commit

---
+++ lib/el_center_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ElCenterApp extends StatelessWidget {
+  const ElCenterApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:e_learning_app/el_center_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ElCenterApp()); 
+  
+}
